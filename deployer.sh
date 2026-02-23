db80ef8e86d36029dc257c0f093dc286ac3bdda1
teddy cubaka <teddycubaka96@gmail.com>
2026-02-23 09:43:51 +0100

    correct coorrectly the docker images

--- deployer.sh
+++ deployer.sh
@@ -9,4 +9,4 @@ docker build -t gass-group-img .
 docker stop gass-group || true
 docker rm gass-group || true
 
-docker run -d -p 3000:3000 --name gass-group web2
+docker run -d -p 3000:3000 --name gass-group gass-group-img
